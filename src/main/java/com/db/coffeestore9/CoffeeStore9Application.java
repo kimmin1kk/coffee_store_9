2aca28c38d70cae8613fa5d0bcb1dbc835b44a0f
kimmin1kk <rudals1888@naver.com>
2023-12-04 14:47:43 +0900

    initial commit

---
+++ src/main/java/com/db/coffeestore9/CoffeeStore9Application.java
@@ -0,0 +1,13 @@
+package com.db.coffeestore9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeStore9Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoffeeStore9Application.class, args);
+  }
+
+}
