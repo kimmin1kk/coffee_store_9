4cd8e92a48b5ac86528167372e865d6ce5b2830c
kimmin1kk <rudals1888@naver.com>
2023-12-04 21:56:23 +0900

    feat: BaseEntity 추가 - BaseTimeEntity 상속 설정 - 실행 클래스 @EnableJpaAuditing 어노테이션 추가

---
+++ src/main/java/com/db/coffeestore9/config/BaseEntity.java
@@ -0,0 +1,48 @@
+package com.db.coffeestore9.config;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.util.Objects;
+import lombok.Getter;
+import org.hibernate.proxy.HibernateProxy;
+
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long seq;
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    if (!(o instanceof BaseEntity that)) {
+      return false;
+    }
+    Class<?> oEffectiveClass = o instanceof HibernateProxy hibernateProxy
+        ? hibernateProxy.getHibernateLazyInitializer()
+        .getPersistentClass() : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy hibernateProxy
+        ? hibernateProxy.getHibernateLazyInitializer()
+        .getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) {
+      return false;
+    }
+    return getSeq() != null && Objects.equals(getSeq(), that.getSeq());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy hibernateProxy
+        ? hibernateProxy.getHibernateLazyInitializer()
+        .getPersistentClass().hashCode() : getClass().hashCode();
+  }
+}
--- src/main/java/com/db/coffeestore9/config/BaseTimeEntity.java
+++ src/main/java/com/db/coffeestore9/config/BaseTimeEntity.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseTimeEntity {
+public abstract class BaseTimeEntity extends BaseEntity {
   @CreatedDate
   private Timestamp createdDate;
 
