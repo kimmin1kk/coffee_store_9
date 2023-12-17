200d72f8b2ae03c10334bd107e0b964e889d008a
kimmin1kk <rudals1888@naver.com>
2023-12-17 20:20:22 +0900

    feat: 포인트사용내역 컨트롤러에 추가

--- src/main/java/com/db/coffeestore9/group/repository/PointUsageRepository.java
+++ src/main/java/com/db/coffeestore9/group/repository/PointUsageRepository.java
@@ -1,8 +1,11 @@
 package com.db.coffeestore9.group.repository;
 
+import com.db.coffeestore9.group.domain.GroupCard;
 import com.db.coffeestore9.group.domain.PointUsage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PointUsageRepository extends JpaRepository<PointUsage, Long> {
 
+  List<PointUsage> findPointUsagesByGroupCard(GroupCard groupCard);
 }
