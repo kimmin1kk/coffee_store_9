9616fd0c7f82bd85041daad8f9ab71e77c47fcc7
kimmin1kk <rudals1888@naver.com>
2023-12-17 19:08:18 +0900

    feat: header <- Controller 모델 추가 fix: 시작되지 않은 랭킹 이벤트 있을 때 랭킹 페이지 접근 안되던 버그 수정 2

--- src/main/java/com/db/coffeestore9/home/controller/HomeController.java
+++ src/main/java/com/db/coffeestore9/home/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.db.coffeestore9.home.controller;
 
+import com.db.coffeestore9.group.service.GroupCardService;
+import com.db.coffeestore9.group.service.GroupUserService;
 import com.db.coffeestore9.product.domain.Product;
 import com.db.coffeestore9.product.service.ProductService;
 import java.security.Principal;
@@ -15,14 +17,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final ProductService productService;
+  private final ProductService productService;
+  private final GroupCardService groupCardService;
+  private final GroupUserService groupUserService;
 
-    @GetMapping("/")
-    public String home(Model model, Principal principal) {
-        List<Product> displayProducts = productService.getProductList();
-        model.addAttribute("products", displayProducts);
+  @GetMapping("/")
+  public String home(Model model, Principal principal) {
+    List<Product> displayProducts = productService.getProductList();
+    model.addAttribute("products", displayProducts);
 
-        return "index";
-    }
+    return "index";
+  }
 
+  @GetMapping("/layouts/header")
+  public String header(Model model, Principal principal) {
+
+      if (groupCardService.getGroupCard(principal.getName()) != null) {
+        model.addAttribute("groupAdminState", groupUserService.getMyGroupUser(
+            principal.getName()).isAdministrator());
+      } else {
+        model.addAttribute("groupState", null);
+      }
+
+    return "layouts/header";
+  }
 }
